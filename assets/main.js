e9b3a4533cdeaec9378308f9fa54d67d2ecef6d7
Juan David Santamaria <juanda404@gmail.com>
2024-02-15 17:04:05 -0500

    hemos finalizado el proyecto

--- assets/main.js
+++ assets/main.js
@@ -21,19 +21,14 @@ async function fetchData(urlApi){
     const videos=await fetchData(API);
     let view=`
     ${videos.items.map(video=>`
-    <a href="https://youtube.com/watch?v=${video.id.videoId}"target ="blank">  
-    <div class="group relative">
-        <div
-          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-        </div>
-        <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
-            <span aria-hidden="true" class="absolute inset-0"></span>
+    <div class="exercise">
+      <a href="https://youtube.com/watch?v=${video.id.videoId}"target ="blank">     
+        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}">
+        <p>
             ${video.snippet.title}
-          </h3>
-        </div>
-      </div>
+        </p>
+        </a>
+    </div>
     `).slice(0,12).join('')}
     `;
     content.innerHTML = view;
